0890b099b31a4f003f038be5d99d961e06603ab2
Dídac Sementé Fernández <didac@ajuna.io>
2024-02-15 11:05:12 +0100

    * Initial commit

---
+++ .maintain/weights.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+set -e
+
+RUNTIME="bajun"
+
+# common pallets shared by both networks
+PALLETS=(
+  "cumulus-pallet-xcmp-queue"
+  "frame-system"
+  "pallet-balances"
+  "pallet-collator-selection"
+  "pallet-collective"
+  "pallet-identity"
+  "pallet-membership"
+  "pallet-multisig"
+  "pallet-nfts"
+  "pallet-preimage"
+  "pallet-proxy"
+  "pallet-scheduler"
+  "pallet-session"
+  "pallet-timestamp"
+  "pallet-treasury"
+  "pallet-utility"
+)
+
+cd "$(git rev-parse --show-toplevel)" || exit
+cargo build-"${RUNTIME}"-benchmarks --features "experimental"
+
+for PALLET in "${PALLETS[@]}"; do
+  ./target/release/"${RUNTIME}"-para benchmark pallet \
+    --chain=dev \
+    --steps=50 \
+    --repeat=20 \
+    --pallet="${PALLET}" \
+    --extrinsic="*" \
+    --execution=wasm \
+    --wasm-execution=compiled \
+    --heap-pages=4096 \
+    --header="./HEADER-AGPL" \
+    --output="./runtime/${RUNTIME}/src/weights/${PALLET//-/_}.rs"
+done
+
+# custom pallets for Bajun network
+[ "${RUNTIME}" != "bajun" ] && exit 0
+CUSTOM_PALLETS=(
+  "pallet-ajuna-awesome-avatars"
+)
+for PALLET in "${CUSTOM_PALLETS[@]}"; do
+  ./target/release/"${RUNTIME}"-para benchmark pallet \
+    --chain=dev \
+    --steps=50 \
+    --repeat=20 \
+    --pallet="${PALLET}" \
+    --extrinsic="*" \
+    --execution=wasm \
+    --wasm-execution=compiled \
+    --heap-pages=4096 \
+    --template="./.maintain/frame-weight-template.hbs" \
+    --output="./runtime/${RUNTIME}/src/weights/${PALLET//-/_}.rs"
+done
---
+++ README.md
@@ -0,0 +1,11 @@
+<p align="center" width="100%">
+  <a href="https://ajuna.io" target="_blank">
+    <img src="docs/ajuna-banner.jpeg" alt="Ajuna Network">
+  </a>
+</p>
+
+[![Build](https://github.com/ajuna-network/ajuna-runtimes-bajun/actions/workflows/check-pull-request.yml/badge.svg?branch=main)](https://github.com/ajuna-network/Ajuna/actions/workflows/check-pull-request.yml)
+[![codecov](https://codecov.io/gh/ajuna-network/ajuna-runtimes-bajun/branch/main/graph/badge.svg?token=V2Y88ZUD6C)](https://codecov.io/gh/ajuna-network/Ajuna)
+[![Docker Image Version (latest semver)](https://img.shields.io/docker/v/ajuna/parachain-bajun?label=bajun%20network&logo=docker&sort=semver&style=plastic)](https://hub.docker.com/repository/docker/ajuna/parachain-bajun/tags?page=1&ordering=last_updated)
+
+A game platform [parachain](https://wiki.polkadot.network/docs/learn-parachains) built with [Substrate](https://docs.substrate.io/).
